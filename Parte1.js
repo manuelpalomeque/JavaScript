8aa497326e2bb2892020241b9751781ee4cd4eec
Palomeque Jonathan Manuel <pjonathanmanuel@hotmail.com>
2021-11-12 10:52:48 -0300

    Incorporé ejercicios

--- Parte1.js
+++ Parte1.js
@@ -29,3 +29,10 @@ function devolverString(str) {
   // Tu código:
   return str ;
 }
+
+function suma(x, y) {
+  // "x" e "y" son números
+  // Suma "x" e "y" juntos y devuelve el valor
+  // Tu código:
+  return  x + y ;
+}
