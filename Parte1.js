1506808345aca120464b04566df01608ae2e4b8b
Palomeque Jonathan Manuel <pjonathanmanuel@hotmail.com>
2021-11-12 11:57:20 -0300

    Incorporé ejercicios

--- Parte1.js
+++ Parte1.js
@@ -54,3 +54,13 @@ function divide(x, y) {
   // Tu código:
   return x / y ;
 }
+
+function sonIguales(x, y) {
+  // Devuelve "true" si "x" e "y" son iguales
+  // De lo contrario, devuelve "false"
+  // Tu código:
+  if ( x === y){
+    return true ;
+  }
+   return false ;
+}
