7de252fe01bb29fe984b0ca196944ae2a8e5f3ae
Palomeque Jonathan Manuel <pjonathanmanuel@hotmail.com>
2021-11-13 09:37:00 -0300

    Incorporé ejercicios

--- Parte1.js
+++ Parte1.js
@@ -139,3 +139,9 @@ function elevar(num, exponent) {
   // Tu código:
   return Math.pow(num, exponent);
 }
+
+function redondearNumero(num) {
+  // Redondea "num" al entero más próximo y devuélvelo
+  // Tu código:
+  return Math.round(num);
+}
