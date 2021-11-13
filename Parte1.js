ce80588eb0775994fd1a5817da5aa1e2a3cb4024
Palomeque Jonathan Manuel <pjonathanmanuel@hotmail.com>
2021-11-13 09:29:42 -0300

    Incorporé ejercicios

--- Parte1.js
+++ Parte1.js
@@ -64,3 +64,13 @@ function sonIguales(x, y) {
   }
    return false ;
 }
+
+function tienenMismaLongitud(str1, str2) {
+  // Devuelve "true" si las dos strings tienen la misma longitud
+  // De lo contrario, devuelve "false"
+  // Tu código:
+  if (str1.length === str2.length){
+    return true ;
+  }
+  return false ;
+}
