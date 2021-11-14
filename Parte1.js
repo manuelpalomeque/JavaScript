3212170804073ce3536fda86e019243c72c4573e
Palomeque Jonathan Manuel <pjonathanmanuel@hotmail.com>
2021-11-14 08:28:49 -0300

    Incorporé ejercicios

--- Parte1.js
+++ Parte1.js
@@ -181,3 +181,10 @@ function agregarSimboloExclamacion(str) {
   str = str + '!';
   return str ;
 }
+
+function combinarNombres(nombre, apellido) {
+  // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
+  // Ejemplo: "Soy", "Henry" -> "Soy Henry"
+  // Tu código:
+  return nombre + ' ' + apellido ;
+}
