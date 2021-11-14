bec669c087ff508ff9762901632d3de33f108530
Palomeque Jonathan Manuel <pjonathanmanuel@hotmail.com>
2021-11-14 08:27:54 -0300

    Incorporé ejercicios

--- Parte1.js
+++ Parte1.js
@@ -173,3 +173,11 @@ function esPositivo(numero) {
     return false ;
   }
 }
+
+function agregarSimboloExclamacion(str) {
+  // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
+  // Ejemplo: "hello world" pasaría a ser "hello world!"
+  // Tu código:
+  str = str + '!';
+  return str ;
+}
