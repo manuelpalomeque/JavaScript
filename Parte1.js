13a2aea416217ad6c9dc2cb548e6657c8b0752eb
Palomeque Jonathan Manuel <pjonathanmanuel@hotmail.com>
2021-11-15 08:48:50 -0300

    Incorpore ejercicios

--- Parte1.js
+++ Parte1.js
@@ -201,3 +201,9 @@ function obtenerAreaRectangulo(alto, ancho) {
   // Tu código:
   return alto * ancho ;
 }
+
+function retornarPerimetro(lado){
+  //Escibe una función a la cual reciba el valor del lado de un cuadrado y retorne su perímetro.
+  //Escribe tu código aquí
+  return lado * 4 ;
+}
