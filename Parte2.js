91eff08f7db2f9f68a6452b68f9d542d659bed8e
Palomeque Jonathan Manuel <pjonathanmanuel@hotmail.com>
2021-11-16 16:39:54 -0300

    Incorpore ejercicios al archivo Parte2

--- Parte2.js
+++ Parte2.js
@@ -67,3 +67,37 @@ function saludo(idioma) {
     return "Hola!";
   }
 }
+
+function colors(color) {
+  //La función recibe un color. Devolver el string correspondiente:
+  //En caso que el color recibido sea "blue", devuleve --> "This is blue"
+  //En caso que el color recibido sea "red", devuleve --> "This is red"
+  //En caso que el color recibido sea "green", devuleve --> "This is green"
+  //En caso que el color recibido sea "orange", devuleve --> "This is orange"
+  //Caso default: devuelve --> "Color not found"
+  //Usar el statement Switch.
+
+  var color = ' red'
+
+  switch (color) {
+    case 'blue' :
+      return 'This is blue';
+      break;
+
+    case 'red' :
+      return 'This is red';
+      break;
+
+    case 'green' :
+      return 'This is green';
+      break;
+
+    case 'orange':
+      return 'This is orange';
+      break;
+
+    default:
+      return 'Color not found'
+      break;
+  }
+}
