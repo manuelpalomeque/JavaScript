6259608e66e364a04b68dfde03da8f4fb9ad6bfb
Palomeque Jonathan Manuel <pjonathanmanuel@hotmail.com>
2021-11-16 16:30:57 -0300

    Incorpore ejercicios al archivo Parte2

---
+++ Parte2.js
@@ -0,0 +1,17 @@
+function obtenerMayor(x, y) {
+  // "x" e "y" son números enteros (int).
+  // Devuelve el número más grande
+  // Si son iguales, devuelve cualquiera de los dos
+  // Tu código:
+
+  if( x > y){
+    return x ;
+  }
+  else if ( y > x){
+    return y ;
+  }
+  else {
+    return x;
+  }
+}
+
