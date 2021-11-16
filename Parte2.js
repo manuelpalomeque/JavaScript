f2fe1899684e2d76e76b945b007b193cd3a1476f
Palomeque Jonathan Manuel <pjonathanmanuel@hotmail.com>
2021-11-16 16:40:28 -0300

    Incorpore ejercicios al archivo Parte2

--- Parte2.js
+++ Parte2.js
@@ -101,3 +101,16 @@ function colors(color) {
       break;
   }
 }
+
+function esDiezOCinco(numero) {
+  // Devuelve "true" si "numero" es 10 o 5
+  // De lo contrario, devuelve "false"
+  // Tu código:
+
+  if(numero === 10 || numero === 5){
+    return true ;
+  }
+  else{
+    return false ;
+  }
+}
