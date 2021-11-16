f66ce51a9cade2d08eb6a6d2f3cd3caba28cedf8
Palomeque Jonathan Manuel <pjonathanmanuel@hotmail.com>
2021-11-16 16:38:33 -0300

    Incorpore ejercicios al archivo Parte2

--- Parte2.js
+++ Parte2.js
@@ -27,3 +27,21 @@ function mayoriaDeEdad(edad) {
     return 'Not allowed';
   }
 }
+
+function conection(status) {
+  //Recibimos un estado de conexión de un usuario representado por un valor numérico.
+  //Cuando el estado es igual a 1, el usuario está "Online"
+  //Cuando el estado es igual a 2, el usuario está "Away"
+  //De lo contrario, presumimos que el usuario está "Offline"
+  //Devolver el estado de conexión de usuario en cada uno de los casos.
+
+  if(status === 1){
+    return 'Online';
+  }
+  else if(status === 2){
+    return 'Away';
+  }
+  else{
+    return 'Offline';
+  }
+}
