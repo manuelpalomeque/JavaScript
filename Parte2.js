48d3df8f8301aac7460adb0d7395e9a8229c2478
Palomeque Jonathan Manuel <pjonathanmanuel@hotmail.com>
2021-11-16 16:41:19 -0300

    Incorpore ejercicios al archivo Parte2

--- Parte2.js
+++ Parte2.js
@@ -114,3 +114,15 @@ function esDiezOCinco(numero) {
     return false ;
   }
 }
+
+function estaEnRango(numero) {
+  // Devuelve "true" si "numero" es menor que 50 y mayor que 20
+  // De lo contrario, devuelve "false"
+  // Tu código:
+  if(numero < 50 && numero > 20){
+    return true ;
+  }
+  else{
+    return false;
+  }
+}
