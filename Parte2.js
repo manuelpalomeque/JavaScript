3046b55115156be0305202804583ffe71208df94
Palomeque Jonathan Manuel <pjonathanmanuel@hotmail.com>
2021-11-17 08:05:20 -0300

    Incorpore ejercicios al archivo Parte2

--- Parte2.js
+++ Parte2.js
@@ -170,3 +170,19 @@ function fizzBuzz(numero) {
     return numero ;
   }
 }
+
+function operadoresLogicos(num1, num2, num3) {
+  //La función recibe tres números distintos.
+  //Si num1 es mayor a num2 y a num3 y además es positivo, retornar ---> "Número 1 es mayor y positivo"
+  //Si alguno de los tres números es negativo, retornar ---> "Hay negativos"
+  //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
+  //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
+  //Si no se cumplen ninguna de las condiciones anteriores, retornar false.
+
+  if(num1 < 0 || num2 < 0 || num3 < 0 ){
+    return 'Hay negativos';
+  }
+
+ else if(num1 === 0 || num2 === 0 || num3 === 0){
+    return 'Error';
+  }
