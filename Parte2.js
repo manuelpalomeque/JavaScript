9da6d71de2bab540df432a2701267aea29dbefd5
Palomeque Jonathan Manuel <pjonathanmanuel@hotmail.com>
2021-11-18 21:36:41 -0300

    Incorpore ejercicios al archivo Parte2

--- Parte2.js
+++ Parte2.js
@@ -203,3 +203,29 @@ CUIDADO CON EL ORDEN */
   }
 
 }
+
+function esPrimo(numero) {
+  // Devuelve "true" si "numero" es primo
+  // De lo contrario devuelve "falso"
+  // Pista: un número primo solo es divisible por sí mismo y por 1
+  // Pista 2: Puedes resolverlo usando un bucle `for`
+  // Nota: Los números 0 y 1 NO son considerados números primos
+
+  if(numero < 2){
+    return false;
+    }
+    // sirve para descartar rapidamente
+
+  if(numero === 2){
+    return true;
+  }
+    // para aprobar rapidamente y desde 2 inicializar la variable en for
+
+  for(var x = 2 ; x < numero ; x++ ){
+      if( numero %  x === 0 ){
+        return false; // no deberia ser true aca ???
+        }
+      }
+      return true;
+
+  }
