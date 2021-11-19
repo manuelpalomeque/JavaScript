5c913f44ca2bc6755e91ab8f5ffb2061a8d01eee
Palomeque Jonathan Manuel <pjonathanmanuel@hotmail.com>
2021-11-19 17:11:32 -0300

    Incorpore ejercicios en la Parte 3

---
+++ Parte3.js
@@ -0,0 +1,6 @@
+function devolverPrimerElemento(array) {
+  // Devuelve el primer elemento de un array
+  // Tu código:
+  return array[0]; // el elemento entre llaves indica el indice del valor a devolver
+}
+
