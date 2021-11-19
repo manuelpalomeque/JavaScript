7e490de43051ac04551abcfe02ffaf6c2a580670
Palomeque Jonathan Manuel <pjonathanmanuel@hotmail.com>
2021-11-19 17:02:27 -0300

    Incorpore ejercicios al archivo Parte2

--- Parte2.js
+++ Parte2.js
@@ -229,3 +229,16 @@ function esPrimo(numero) {
       return true;
 
   }
+
+function esVerdadero(valor){
+  //Escribe una función que reciba un valor booleano y retorne “Soy verdadero”
+  //si su valor es true y “Soy falso” si su valor es false.
+  //Escribe tu código aquí
+
+  if(valor === true){
+    return 'Soy verdadero';
+  }
+  else{
+    return 'Soy falso';
+  }
+}
