f2fe2e136005dd7197ccb6265edb11ff1f9a2f05
Palomeque Jonathan Manuel <pjonathanmanuel@hotmail.com>
2021-11-21 10:57:34 -0300

    Incorpore ejercicios en la Parte3

--- Parte3.js
+++ Parte3.js
@@ -51,3 +51,12 @@ function agregarItemAlComienzoDelArray(array, elemento) {
   array.unshift(elemento);
   return array ;
 }
+
+function dePalabrasAFrase(palabras) {
+  // "palabras" es un array de strings/cadenas
+  // Devuelve un string donde todas las palabras estén concatenadas
+  // con espacios entre cada palabra
+  // Ejemplo: ['Hello', 'world!'] -> 'Hello world!'
+  // Tu código:
+   return palabras.join(' ') ;
+}
