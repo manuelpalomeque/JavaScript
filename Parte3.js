f53cfaaa9c0ec70176b703bf0f92caa63cbb7f2e
Palomeque Jonathan Manuel <pjonathanmanuel@hotmail.com>
2021-11-21 09:43:16 -0300

    Incorpore ejercicios en la Parte3

--- Parte3.js
+++ Parte3.js
@@ -15,3 +15,16 @@ function obtenerLargoDelArray(array) {
   // Tu código:
   return array.length ;
 }
+
+function incrementarPorUno(array) {
+  // "array" debe ser una matriz de enteros (int/integers)
+  // Aumenta cada entero por 1
+  // y devuelve el array
+  // Tu código:
+  var nuevoArray = [];
+
+  for(let x = 0 ; array.length > x ; x++){
+    nuevoArray[x] = array[x] + 1;
+  }
+  return nuevoArray;
+}
