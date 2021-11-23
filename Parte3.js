7e0eed4682725309b8e94b7f81e85ad16a65e9ea
Palomeque Jonathan Manuel <pjonathanmanuel@hotmail.com>
2021-11-23 19:12:32 -0300

    Incorpore ejercicios en la Parte3

--- Parte3.js
+++ Parte3.js
@@ -160,3 +160,18 @@ function diaDeLaSemana(numeroDeDia) {
 
 }
 
+function empiezaConNueve(n) {
+  //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero
+  //inicia con 9 y false en otro caso.
+  //Escribe tu código aquí
+
+  let numero = n.toString();
+
+  if( numero.charAt(0) === '9'){
+    return true;
+   }
+  else{
+     return false;
+   }
+
+}
