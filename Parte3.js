f86c7879e79e8ad27c57c70cdc328c16213837e7
Palomeque Jonathan Manuel <pjonathanmanuel@hotmail.com>
2021-11-24 16:15:19 -0300

    Incorpore ejercicios en la Parte3

--- Parte3.js
+++ Parte3.js
@@ -209,3 +209,17 @@ function mesesDelAño(array) {
       return nuevoArray; //si estan los tres meses , devulvo el array
   }
 }
+
+function mayorACien(array) {
+  //La función recibe un array con enteros entre 0 y 200. Recorrer el array y guardar en un nuevo array sólo los
+  //valores mayores a 100 (no incluye el 100). Finalmente devolver el nuevo array.
+  // Tu código:
+  let nuevoArray2 =[] ;
+
+  for( let x = 0 ; x < array.length ; x++){
+    if(array[x] > 100){
+      nuevoArray2.push(array[x]);
+    }
+  }
+  return nuevoArray2
+}
