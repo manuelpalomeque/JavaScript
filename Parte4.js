0de0e6178dde8b5624ae6d2df9a8484e696eb111
manuelpalomeque <pjonathanmanuel@hotmail.com>
2021-11-30 16:59:06 -0300

    Incorpore mas ejercicios a parte4

--- Parte4.js
+++ Parte4.js
@@ -244,3 +244,30 @@ function sumarLikesDeUsuario(usuario) {
   return suma;
 
 }
+
+function agregarMetodoCalculoDescuento(producto) {
+  // Agregar un método (función) al objeto "producto" llamado "calcularPrecioDescuento"
+  // Este método debe multiplicar el "precio" del "producto" ("producto.precio" o "producto[precio]") y "porcentajeDeDescuento" para obtener el descuento
+  // El método resta el descuento del precio y devuelve el precio con descuento
+  // Devuelve el objeto "producto" al final de la función
+  // Ejemplo:
+  // producto.precio -> 20
+  // producto.porcentajeDeDescuento -> 0.2 (o simplemente ".2")
+  // producto.calcularPrecioDescuento() -> 20 - (20 * 0.2)
+  // Tu código:
+
+  /*
+    var producto = {
+      precio: xxxx,
+      porcentajeDeDescuento: xxxxx,
+      calcularPrecioDeDescuento: function() {
+        return this.precio - (this.precio * this.porcentajeDeDescuento)
+      }
+    }
+  */
+
+  producto.calcularPrecioDescuento = function() {
+      return this.precio - (this.precio * this.porcentajeDeDescuento) ;
+  };
+  return producto;
+}
