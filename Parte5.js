271092cda4f6da7949228aedb971922f0326459d
manuelpalomeque <pjonathanmanuel@hotmail.com>
2021-12-01 15:52:55 -0300

    Incorporé ejercicios en parte 5

--- Parte5.js
+++ Parte5.js
@@ -21,3 +21,13 @@ function crearUsuario() {
 
   return Usuario;
 }
+
+function agregarMetodoPrototype(Constructor) {
+  // Agrega un método al Constructor del `prototype`
+  // El método debe llamarse "saludar" y debe devolver la string "Hello World!"
+  // Tu código:
+
+  Constructor.prototype.saludar = function(){
+    return 'Hello World!' ;
+  }
+}
