b4baa93e620f60c333797957f3bc293d396dda73
manuelpalomeque <pjonathanmanuel@hotmail.com>
2021-12-02 19:57:35 -0300

    Incorporé ejercicios en parte 5

--- Parte5.js
+++ Parte5.js
@@ -48,3 +48,33 @@ function agregarStringInvertida() {
     return stringInvertida;
   };
 }
+
+// ---------------------------------------------------------------------------//
+  //Crea el constructor de la clase "Persona"
+  //Debe tener las propiedades: "nombre", "apellido", "edad" y "domicilio"
+  //Debe tener un método llamado "detalle" que nos devuelve un objeto con las propiedades de la persona y sus valores.
+  //Ej: {
+    //   Nombre: 'Juan',
+    //   Apellido: 'Perez',
+    //   Edad: 22,
+    //   Domicilio: 'Saavedra 123'
+    //  }
+
+  class Persona {
+    constructor(nombre, apellido, edad, domicilio) {
+      // Tu código:
+      this.nombre = nombre,
+      this.apellido = apellido,
+      this.edad = edad,
+      this.domicilio = domicilio
+      this.detalle = function() {
+        return {
+          Nombre: this.nombre,
+          Apellido: this.apellido,
+          Edad: this.edad,
+          Domicilio: this.domicilio
+          }
+        }
+      }
+}
+
